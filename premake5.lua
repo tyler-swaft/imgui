a8cedf5011e4719529f3f8188dcd0fb128108cd8
tyler-swaft <77362661+tyler-swaft@users.noreply.github.com>
2024-07-14 16:29:39 +0100

    add premake file

---
+++ premake5.lua
@@ -0,0 +1,35 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "on"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-inter/" .. outputdir .. "/%{prj.name}")
+
+    files 
+    {
+        "imconfig.h",
+        "imgui.cpp",
+        "imgui.h",
+        "imgui_demo.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_tables.cpp",
+        "imgui_widgets.cpp",
+        "imgui_rectpack.h",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
